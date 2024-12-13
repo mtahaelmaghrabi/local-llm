9abb87ef4e2cf744a05190a7492f20c2a1b3e583
Mohamed Taha <mtaha@pp.gov.qa>
2024-12-13 18:53:49 +0300

    stream

--- fastapi/app.py
+++ fastapi/app.py
@@ -12,8 +12,24 @@ def home():
 def ask(prompt :str):
     res = requests.post('http://ollama:11434/api/generate', json={
         "prompt": prompt,
-        "stream" : True,
+        "stream" : False,
         "model" : "llama3.3:70b"
     })
 
     return Response(content=res.text, media_type="application/json")
+
+
+# from fastapi import FastAPI, Response
+# import httpx
+
+# app = FastAPI()
+
+# @app.get('/ask')
+# async def ask(prompt: str):
+#     async with httpx.AsyncClient() as client:
+#         response = await client.post('http://ollama:11434/api/generate', json={
+#             "prompt": prompt,
+#             "stream": False,
+#             "model": "llama3.3:70b"
+#         })
+#     return Response(content=response.text, media_type="application/json")
